f8acd7d6e1a77d1415f988a9679455ad34136f9f
JundanJauhar <jundan87@gmail.com>
2025-10-30 14:11:40 +0700

    perbaikan template

---
+++ resources/views/admin/template/sidebar.blade.php
@@ -0,0 +1,140 @@
+<div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar"
+    data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="225px"
+    data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
+    <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
+        <img alt="Logo" src="assets/media/logos/landing.png" class="h-30px app-sidebar-logo-default" />
+        <div id="kt_app_sidebar_toggle"
+            class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary h-30px w-30px position-absolute top-50 start-100 translate-middle rotate"
+            data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body"
+            data-kt-toggle-name="app-sidebar-minimize">
+            <i class="ki-duotone ki-black-left-line fs-3 rotate-180">
+                <span class="path1"></span>
+                <span class="path2"></span>
+            </i>
+        </div>
+    </div>
+    <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
+        <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper">
+            <div id="kt_app_sidebar_menu_scroll" class="scroll-y my-5 mx-3" data-kt-scroll="true"
+                data-kt-scroll-activate="true" data-kt-scroll-height="auto"
+                data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer"
+                data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px"
+                data-kt-scroll-save-state="true">
+                <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold fs-6" id="#kt_app_sidebar_menu"
+                    data-kt-menu="true" data-kt-menu-expand="false">
+                    <div class="menu-item">
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-abstract-33 fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </div>
+                    <div class="menu-item pt-5">
+                        <div class="menu-content">
+                            <span class="menu-heading fw-bold text-uppercase fs-7">Pages</span>
+                        </div>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-book-open fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Violations</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-filter fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Violation Categories</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-note-2 fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Sanctions</span>
+                        </a>
+                    </div>
+                    <div class="menu-item active">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-teacher fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Students</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-flag fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Classes</span>
+                        </a>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-user fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Users</span>
+                        </a>
+                    </div>
+                    <div class="menu-item pt-5">
+                        <div class="menu-content">
+                            <span class="menu-heading fw-bold text-uppercase fs-7">Account</span>
+                        </div>
+                    </div>
+                    <div class="menu-item">
+
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-setting fs-2">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                            </span>
+                            <span class="menu-title">Edit Account</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
+        <!-- <form action="/logout" method="POST">
+            @csrf
+            <button type="submit"
+                class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100"
+                data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click">
+                <span class="btn-label">Logout</span>
+                <i class="ki-duotone ki-document btn-icon fs-2 m-0">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>
+            </button>
+        </form> -->
+    </div>
+</div>
---
+++ resources/views/admin/test.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.template.main')
+
+@section('title', 'Dashboard Admin - EduGuard')
+
+<!-- @section('content')
+    <div><p>test</p></div> -->
+<!-- @endsection -->
+
